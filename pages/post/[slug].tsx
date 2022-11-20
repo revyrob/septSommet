938b6cf88cedb315a74b41759eef6062c5ecf4d1
revyrob <kayle.robson@gmail.com>
2022-11-20 08:20:47 -0800

    added posts single page to application

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -7,15 +7,27 @@ interface Props {
   post:Post;
 }
 
+//this shows each post on its own page
 function Post({post}: Props) {
+  console.log(new Date(post._createAt).toLocaleDateString())
   console.log(post)
   return (
     <main>
       <Header />
+      <img className="w-full h-40 object-cover" src={urlFor(post.mainImage).url!()} alt={post.title}/>
+      <article className="max-w-3xl mx-auto p-5">
+        <h1 className="text-3xl mt-10 mb-3">{post.title}</h1>
+        <h2 className="text-xl font-light text-gray-500 mb-2">{post.description}</h2>
+
+        <div>
+          <img className="h-10 w-10 rounded-full" src={urlFor(post.author.image).url!()} alt={post.author.name}/>
+          <p className="font-extralight text-sm">Blog post by {post.author.name} - Published at {new Date(post._createAt).toLocaleString()}</p>
+          
+        </div>
+      </article>
     </main>
   );
 }
-
 export default Post;
 
 export const getStaticPaths = async () => {
@@ -75,5 +87,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     props: {
       post,
     },
+    //keep updating the cash, after 60seconds it will update the cache
+    revalidate:60, 
   };
 };
